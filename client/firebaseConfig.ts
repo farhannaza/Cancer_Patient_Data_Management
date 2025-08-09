0d736f024e7869410d35a14b0aed33f8ab3de1c0
farhan <farhan.selangor@gmail.com>
2025-08-09 11:32:32 +0800

    Update firebaseConfig.ts

--- client/firebaseConfig.ts
+++ client/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database";
 import { json, LoaderFunction } from "@remix-run/node";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyABd0P58Guvh0GyY08BuWnccZPnzxHerdw",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "onchologychain-ff1d9.firebaseapp.com",
-  databaseURL: process.env.FIREBASE_DATABASE_URL || "https://onchologychain-ff1d9-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: process.env.FIREBASE_PROJECT_ID || "onchologychain-ff1d9",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "onchologychain-ff1d9.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "249856462590",
-  appId: process.env.FIREBASE_APP_ID || "1:249856462590:web:9ce5383e04768ac38d9faf"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN ,
+  databaseURL: process.env.FIREBASE_DATABASE_URL ,
+  projectId: process.env.FIREBASE_PROJECT_ID ,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.FIREBASE_APP_ID 
 };
 
 // Initialize Firebase
